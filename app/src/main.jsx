7ad8e4a92b70bd8f340333d59aae1afc51b6157c
messagerangelhelper-code <messagerangelhelper@gmail.com>
2026-09-06 11:09:59 -0500

    Update main.jsx

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div>
-      <h1>ScamShield</h1>
-      <p>Scam detection and victim protection.</p>
-    </div>
-  );
-}
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
