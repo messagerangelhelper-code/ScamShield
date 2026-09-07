ec896c1fdbb2905dd6fac7026d2b49862aff3883
messagerangelhelper-code <messagerangelhelper@gmail.com>
2026-09-07 07:51:27 -0500

    Update App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,6 @@
 import Dashboard from "./components/Dashboard";
 import ScamCheck from "./components/ScamCheck";
-import Subscriptions from "./pages/Subscriptions";
+
 function App() {
   return (
     <main>
